227d9891bb78badd53d5c116c8a069aa66863e3f
chendongdong <564964001@qq.com>
2020-12-05 23:46:05 +0800

    1.设置模版 2.导入依赖模块，分离并且加载依赖文件

--- src/index.js
+++ src/index.js
@@ -1,3 +1,81 @@
-export function a() {
-    console.log(1,angular.version)
-}
+// 导入依赖模块对象
+import * as angular from 'angular';
+import * as angularRoute from 'angular-route';
+import * as angularSanitize from 'angular-sanitize';
+import * as oclazyload from 'oclazyload';
+//
+// export function a() {
+//     console.log(1,angular.version)
+// }
+const App = angular
+    .module("app",['ngRoute','ngSanitize','oc.lazyLoad'])
+    // .factory("layuis",function () {
+    //     const lu = {
+    //         layer:{},
+    //         aLayer:function () {
+    //             return new Promise(function (resolve) {
+    //                 layui.use(["layer"], function () {
+    //                     resolve(layui.layer)
+    //                 });
+    //             })
+    //         }};
+    //     layui.use(["layer"], function () {
+    //         lu.layer = layui.layer;
+    //     });
+    //     return lu;
+    // })
+    .factory('httpInterceptor',function ($q) {
+        return {
+            // 请求的拦截
+            request: function (config) {
+                config.headers = config.headers || {};
+                // let token = JSON.parse(window.localStorage.getItem("token")) || "";
+                // config.headers.Authorization = 'Bearer ' + token.access_token;
+                return config;
+            },
+            // 拦截响应
+            response: function(response) {
+                // console.log("响应");
+                // console.log(response);
+                return response;
+            },
+            // 拦截请求错误
+            responseError:function(errorReason){
+                // console.log('请求错误');
+                // console.log(errorReason);
+                return $q.reject(errorReason);
+            },
+            // 拦截响应错误
+            requestError:function(errorReason){
+                // console.log('拦截响应错误');
+                return $q.reject(errorReason);
+            }
+        };
+    })
+    .config(function ($httpProvider) {
+        $httpProvider.interceptors.push('httpInterceptor');
+    })
+    .run(function () {
+        // 默认创建基础滚动条
+        console.log('run ok')
+        // $(".ScrollBox").mCustomScrollbar({
+        //     theme: "minimal-dark",
+        //     mouseWheel:{scrollAmount:200},
+        //     callbacks:{
+        //         onUpdate:function(){
+        //             // 数据更新后，将滚动条滚动至 top
+        //             $(this).mCustomScrollbar("scrollTo","top");
+        //         }
+        //     }
+        // });
+        // // layui 组件初始化
+        // try {
+        //     if (layer){
+        //
+        //     }
+        // }catch (e) {
+        //     layui.use(['layer'],function (layer) {
+        //         console.info('layer init');
+        //     });
+        // }
+    });
--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,55 @@
 const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    mode: 'development', // 进入指定模式 development or production
-    plugins: [],
+    mode: 'production', // 进入指定模式 development or production
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            title: '开发模式',
+            template: "assets/tpl.angular.html"
+        })
+    ],
     devtool: 'inline-source-map', // 生成 source-map 文件，方便错误提示
     devServer: {
         contentBase: './dist', // 通知 webpack-dev-server 站点根目录地址
     },
     entry: {
-        index: './src/index.js'
+        AngularJsProjectCommonTools: {
+            import: './src/index.js',
+        }
     },
     output: {
         libraryTarget: 'umd',
         library: 'AngularJsProjectCommonTools',
-        filename: 'AngularJsProjectCommonTools.js',
+        filename: '[name].js',
         path: path.resolve(__dirname, 'dist')
     },
     externals: {
-        angular: {
-            commonjs: 'angular',
-            commonjs2: 'angular',
-            amd: 'angular',
-            root: 'angular',
-        },
+        // angular: {
+        //     commonjs: 'angular',
+        //     commonjs2: 'angular',
+        //     amd: 'angular',
+        //     root: 'angular',
+        // },
     },
     module: {
+    },
+    optimization: {
+        runtimeChunk: 'single',
+        splitChunks: {
+            cacheGroups: { // 分组
+                vendor: {
+                    test: /[\\/]node_modules[\\/]/, // 正则路径
+                    name(module, chunks, cacheGroupKey) {
+                        // 获取模块名称
+                        const moduleFileName = module.identifier().split('/').reduceRight(item => item).split('.')[0];
+                        return `${moduleFileName}`;
+                    },
+                    chunks: 'all',
+                },
+            },
+        }
     }
 };

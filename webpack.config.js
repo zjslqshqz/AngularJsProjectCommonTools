43945b066ae54ea25ddee587386f5e0d9d7214cf
chendongdong <564964001@qq.com>
2020-12-05 16:03:11 +0800

    init

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const path = require('path');
+
+module.exports = {
+    mode: 'development', // 进入指定模式 development or production
+    plugins: [],
+    devtool: 'inline-source-map', // 生成 source-map 文件，方便错误提示
+    devServer: {
+        contentBase: './dist', // 通知 webpack-dev-server 站点根目录地址
+    },
+    entry: {
+        index: './src/index.js'
+    },
+    output: {
+        libraryTarget: 'umd',
+        library: 'AngularJsProjectCommonTools',
+        filename: 'AngularJsProjectCommonTools.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+    }
+};
